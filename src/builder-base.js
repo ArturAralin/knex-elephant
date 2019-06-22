13d6cf9fe31e136fe25980df4edcf7216723c7ce
Artur Aralin <artur.aralin97@gmail.com>
2019-06-22 11:11:53 +0300

    add: jsdoc for isKnexQB, isKnexRaw,  knexRaw

--- src/builder-base.js
+++ src/builder-base.js
@@ -17,9 +17,28 @@ const knex = knexLib({ client: 'pg' });
 const QueryBuilder = require('knex/lib/query/builder');
 const Raw = require('knex/lib/raw');
 
+/**
+ * Checking on instance of Knex.QueryBuilder
+ * @private
+ * @param o - some object
+ * @returns {Boolean}
+ */
 const isKnexQB = o => o instanceof QueryBuilder;
+
+/**
+ * Checking on instance of Knex.Raw
+ * @private
+ * @param o - some object
+ * @returns {Boolean}
+ */
 const isKnexRaw = o => o instanceof Raw;
 
+/**
+ * Wrap text into Knex.Raw
+ * @private
+ * @param {String} sql
+ * @returns {Knex.Raw}
+ */
 const knexRaw = sql => knex.raw(sql);
 
 const isAlias = test(/as /i);
