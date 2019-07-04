cc2f19ad636ae4c0c318c1342f9b46deb16348c8
Artur Aralin <artur.aralin97@gmail.com>
2019-07-04 21:48:58 +0300

    update: imports

--- src/index.js
+++ src/index.js
@@ -14,16 +14,24 @@ const { unnest } = require('./builders/unnest');
 const { rowToJson } = require('./builders/row-to-json');
 const {
   jsonToRecordset,
+  jsonbToRecordset,
 } = require('./builders/json-to-recordset');
+const {
+  jsonEach,
+  jsonbEach,
+} = require('./builders/json-each');
 
 module.exports = {
   jsonAgg,
   jsonArrayElements,
   jsonBuildObject,
+  jsonEach,
   jsonToRecordset,
   jsonbAgg,
   jsonbArrayElements,
   jsonbBuildObject,
+  jsonbEach,
+  jsonbToRecordset,
   rowToJson,
   unnest,
 };
